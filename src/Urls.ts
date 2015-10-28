ec644d66449a55279369408bf6e0d6eed164929a
Sebastian Öberg <sebastian.oberg@gmail.com>
2015-10-28 14:55:42 +0100

    Use history API instead if it's available, new function to update the url path.

--- src/Urls.ts
+++ src/Urls.ts
@@ -24,8 +24,38 @@ module Utils {
             if (index != -1) {
                 url = url.substr(0, url.indexOf('#'));
             }
+            
+            if (window.top.history.replaceState)
+                window.top.history.replaceState(null, null, url + newHash);
+            else
+                doc.location.replace(url + newHash);
+        }
+        
+        static SetUrlAfter(searchvalue: string, value: string, doc?: Document): void {
+            if (!doc) doc = window.document;
+            
+            var url = doc.URL;
 
-            doc.location.replace(url + newHash);
+            var searchIndex = url.lastIndexOf(searchvalue);
+            if (searchIndex == -1)
+                return;
+
+            var startUrl = url.substr(0, searchIndex);
+            var endUrl = url.substr(searchIndex);
+
+            var indexAfter = endUrl.indexOf("?");
+            if (indexAfter == -1)
+                indexAfter = endUrl.indexOf("&");
+            if (indexAfter == -1)
+                indexAfter = endUrl.indexOf("#");
+
+            if (indexAfter != -1)
+                endUrl = endUrl.substr(indexAfter);
+            else
+                endUrl = "";
+
+            if (window.top.history.replaceState)
+                window.top.history.replaceState(null, null, startUrl + searchvalue + value + endUrl);
         }
 
         static GetQuerystringParameter(key: string, w?: Window): string {
